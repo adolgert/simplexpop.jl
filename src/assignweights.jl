a167c39e4b7c4f7205b16d953de8329041a5086d
Drew Dolgert <adolgert@uw.edu>
2020-06-16 00:13:59 -0700

    cleared up xy vs ij questions and debugged using a mock band

--- src/assignweights.jl
+++ src/assignweights.jl
@@ -1,6 +1,14 @@
-using ArchGDAL: read, AbstractRasterBand, getdataset, getgeotransform, blocksize, pixeltype,
-        readblock!, writeblock!, getband, width, height, getnodatavalue
+using ArchGDAL: AbstractRasterBand, getdataset, getgeotransform, blocksize, pixeltype,
+        readblock!, writeblock!, getband, getgeotransform, width, height, getnodatavalue
+using ArchGDAL
 using OffsetArrays
+
+
+"""
+Find the origin and linear transform from pixel to x-y.
+"""
+geo_to_transform(geo) = ([geo[1], geo[4]], [geo[2] geo[3]; geo[5] geo[6]])
+
 
 """
     geo_to_xy_corner(origin, transform, i, j)
@@ -29,14 +37,6 @@ geo_to_xy_center(origin, transform, i, j) = (
         )
 
 
-function xy_bounds(origin, transform, ij_dim)
-    ul = geo_to_xy_corner(origin, transform, 1, 1)
-    lr = geo_to_xy_corner(origin, transform, ij_dim[1] + 1, ij_dim[2] + 1)
-    ul, lr
-end
-
-
-
 """
 Which pixel contains this xy point.
 """
@@ -55,11 +55,18 @@ end
 
 """
 The rect is a tuple of (ul, lr).
+This tends to pull in the next pixel when
+the grids are exactly aligned.
 """
 function ij_cover_rect(origin, transform, rect)
     ul_ij = pixel_containing(origin, transform, rect[1])
     lr_ij = pixel_containing(origin, transform, rect[2])
     ul_ij, lr_ij
+end
+
+
+function pointinrect(ij, rect)
+    rect[1][1] <= ij[1] < rect[2][1] && rect[1][2] <= ij[2] < rect[2][2]
 end
 
 
@@ -74,16 +81,14 @@ function corner_pixel(block_idx::CartesianIndex, blocksize)
 end
 
 
-function pixels_of_block(block_idx::CartesianIndex, blocksize)
-    CartesianIndices((
-        ((block_idx[1] - 1) * blocksize[1] + 1):(block_idx[1] * blocksize[1]),
-        ((block_idx[2] - 1) * blocksize[2] + 1):(block_idx[2] * blocksize[2])
-    ))
-end
-
+### PixelGrid methods.
 
 """
-Represents a grid extent and its geometry.
+Represents a grid extent and its geometry, without data.
+
+For GDAL, coordinates are (x-offset, y-offset), and it's stored as
+(row, col) in the array. We think of the x-offset, visually, as the column,
+but forget that. This world is (x-offset, y-offset).
 
 The grid extent is an upper-lefthand `(i0, j0)` and a lower right-hand
 `(i1, j1)`, where `i0 < i1` and `j0 < j1`. This orientation comes from
@@ -97,17 +102,23 @@ Ygeo = GT(3) + Xpixel*GT(4) + Yline*GT(5)
 ```
 """
 struct PixelGrid
-    ul::Tuple{Int64,Int64}
-    lr::Tuple{Int64,Int64}
-    origin::Array{Float64,1}
+    ul::Tuple{Int64,Int64}  # As x, y
+    lr::Tuple{Int64,Int64}  # As x, y
+    origin::Array{Float64,1}  # x, y, too.
     transform::Array{Float64,2}
 end
 
-geo_to_transform(geo) = ([geo[1], geo[4]], [geo[2] geo[3]; geo[5] geo[6]])
 
+"""
+Make a function to get the geotransform so that we can stub the Band during testing.
+"""
+ArchGDAL.getgeotransform(band::AbstractRasterBand) = getgeotransform(getdataset(band))
+
+"""
+Make a PixelGrid that represents all data available in a Band.
+"""
 function pixelgrid(band::AbstractRasterBand)
-    ds = getdataset(band)
-    geo = getgeotransform(ds)
+    geo = getgeotransform(band)
     w = width(band)
     h = height(band)
     origin, transform = geo_to_transform(geo)
@@ -140,8 +151,8 @@ end
 
 
 function crop_to(large::PixelGrid, small::PixelGrid)
-    ul, lr = xy_bounds(small)
-    ul_ij, lr_ij = ij_cover_rect(large.origin, large.transform, (ul, lr))
+    ul_xy, lr_xy = xy_bounds(small)
+    ul_ij, lr_ij = ij_cover_rect(large.origin, large.transform, (ul_xy, lr_xy))
     uln = (max(large.ul[1], ul_ij[1]), max(large.ul[2], ul_ij[2]))
     lrn = (min(large.lr[1], lr_ij[1]), min(large.lr[2], lr_ij[2]))
     PixelGrid(uln, lrn, large.origin, large.transform)
@@ -164,6 +175,53 @@ function onepixelgrid(pg::PixelGrid, pij)
 end
 
 
+
+"""
+Iterate over all blocks that cover a given pixel rectangle.
+"""
+function iter_block_indices(pg::PixelGrid, blocksize)
+    coarse_block_bounds = block_cover_ij_rect((pg.ul, pg.lr), blocksize)
+    i, j = 1, 2
+    CartesianIndices((coarse_block_bounds[1][i]:coarse_block_bounds[2][i],
+            coarse_block_bounds[1][j]:coarse_block_bounds[2][j]))
+end
+
+
+### Stub of GDAL's AbstractRasterBand, for testing.
+
+struct StubBand{T} <: AbstractRasterBand
+    A::Array{T, 2}  # data
+    bs::Array{Int32,1}  # blocksize
+    o::Array{Float64,1}  # origin in xy.
+    t::Array{Float64,2}  # linear transform from pixel to xy.
+end
+
+ArchGDAL.width(b::StubBand) = size(b.A, 1)
+ArchGDAL.height(b::StubBand) = size(b.A, 2)
+ArchGDAL.blocksize(b::StubBand) = b.bs
+ArchGDAL.pixeltype(b::StubBand{T}) where {T} = T
+function ArchGDAL.readblock!(b::StubBand, i::Integer, j::Integer, buffer)
+    @assert 0 <= b.bs[1] * i < width(b)  # beginning of block is less than width.
+    @assert 0 <= b.bs[2] * j < height(b)
+    # The last block will likely extend past the end of the actual data.
+    xaxis = (b.bs[1]*i + 1):min(b.bs[1]*(i+1), size(b.A, 1))
+    yaxis = (b.bs[2]*j + 1):min(b.bs[2]*(j+1), size(b.A, 2))
+    buffer[1:length(xaxis), 1:length(yaxis)] = b.A[xaxis, yaxis]
+end
+ArchGDAL.getnodatavalue(b::StubBand{T}) where {T <: Real} = floatmin(T)
+ArchGDAL.getnodatavalue(b::StubBand{T}) where {T <: Integer} = typemin(T)
+function ArchGDAL.writeblock!(b::StubBand, i::Integer, j::Integer, buffer)
+    @assert 0 <= b.bs[1] * i < width(b)  # beginning of block is less than width.
+    @assert 0 <= b.bs[2] * j < height(b)
+    # The last block will likely extend past the end of the actual data.
+    xaxis = (b.bs[1]*i + 1):min(b.bs[1]*(i+1), size(b.A, 1))
+    yaxis = (b.bs[2]*j + 1):min(b.bs[2]*(j+1), size(b.A, 2))
+    b.A[xaxis, yaxis] = buffer[1:length(xaxis), 1:length(yaxis)]
+end
+ArchGDAL.getgeotransform(b::StubBand) = [b.o[1], b.t[1,1], b.t[1,2], b.o[2], b.t[2,1], b.t[2,2]]
+
+
+### DataGrid methods.
 """
 A DataGrid represents a matrix of raster values.
 
@@ -182,40 +240,48 @@ struct DataGrid{T}
 end
 
 
-function load_pixel_grid(band::AbstractRasterBand, pg::PixelGrid)
+function load_data_grid(band::AbstractRasterBand, pg::PixelGrid)
     bs = blocksize(band)
     ulb, lrb = block_cover_ij_rect((pg.ul, pg.lr), bs)
     dtype = pixeltype(band)
-    A = zeros(dtype, (lrb[1] - ulb[1] + 1) * bs[1], (lrb[2] - ulb[2] + 1) * bs[2])
+    ul_ij = ((ulb[1] - 1) * bs[1] + 1, (ulb[2] - 1) * bs[2] + 1)
+    lr_ij = (lrb[1] * bs[1], lrb[2] * bs[2])
+    @show ul_ij
+    @show lr_ij
+    A = zeros(dtype, lr_ij[1] - ul_ij[1] + 1, lr_ij[2] - ul_ij[2] + 1)
     buffer = zeros(dtype, bs...)
     for bidx in CartesianIndices(((lrb[1] - ulb[1] + 1), (lrb[2] - ulb[2] + 1)))
-        # readblock! is zero-based.
+        # readblock! is zero-based and is col, row b/c it's xaxis, yaxis.
         readblock!(band, bidx[1] - 1, bidx[2] - 1, buffer)
+        # Blocksize is still x, y, so it's flipped here.
         A[((bidx[1] - 1) * bs[1] + 1):(bidx[1] * bs[1]),
             ((bidx[2] - 1) * bs[2] + 1):(bidx[2] * bs[2])] = buffer
     end
-    offset = OffsetArray(
-            A,
-            ((ulb[1] - 1) * bs[1] + 1):(lrb[1] * bs[1]),
-            ((ulb[2] - 1) * bs[2] + 1):(lrb[2] * bs[2]),
-            )
+    offset = OffsetArray(A, ul_ij[1]:lr_ij[1], ul_ij[2]:lr_ij[2])
     nodata = getnodatavalue(band)
     DataGrid{dtype}(offset, nodata, bs, ulb, lrb, pg)
 end
 
 
-function count_nonzero_pixels(dg::DataGrid)
+"""
+Count nonzero pixels that are inside the boundsrect.
+"""
+function count_nonzero_pixels(dg::DataGrid, boundsrect)
     cnt = 0
     ul = dg.pg.ul
     lr = dg.pg.lr
+    # i, j are x, y for GDAL but flipped for the array.
     for j in ul[2]:lr[2]
         for i in ul[1]:lr[1]
             if dg.A[i, j] != dg.nodatavalue
-                cnt += 1
+                pt = geo_to_xy_center(dg.pg.origin, dg.pg.transform, i, j)
+                if pointinrect(pt, boundsrect)
+                    cnt += 1
+                end
             end
         end
     end
-    percent_filled = cnt / ((lr[2] - ul[2]) * (lr[2] - ul[2]))
+    percent_filled = cnt / ((lr[1] - ul[1]) * (lr[2] - ul[2]))
     @show percent_filled
     cnt
 end
@@ -234,11 +300,13 @@ function fill_nonzero_pixels!(dg::DataGrid, value)
 end
 
 
-function write_datagrid(dg::DataGrid, band)
+function write_datagrid!(dg::DataGrid, band)
     bs = dg.blocksize
     for j in dg.ulb[2]:dg.lrb[2]
         for i in dg.ulb[1]:dg.lrb[1]
-            writeblock!(band, i, j, dg.A[(i*bs[1]):((i+1)*bs[1]-1), (j*bs[2]):((j+1)*bs[2]-1)])
+            xrange = ((i - 1) * bs[1] + 1):(i * bs[1])
+            yrange = ((j - 1) * bs[2] + 1):(j * bs[2])
+            writeblock!(band, i-1, j-1, dg.A[xrange, yrange])
         end
     end
 end
@@ -248,7 +316,7 @@ function load_single_block(band::AbstractRasterBand, pg::PixelGrid, block)
     bs = blocksize(band)
     @show bs
     dtype = pixeltype(band)
-    A = zeros(dtype, bs...)
+    A = zeros(dtype, bs[1], bs[2])
     readblock!(band, block[1] - 1, block[2] - 1, A)
     ul = ((block[1] - 1) * bs[1] + 1, (block[2] - 1) * bs[2] + 1)
     lr = (block[1] * bs[1], block[2] * bs[2])
@@ -256,16 +324,6 @@ function load_single_block(band::AbstractRasterBand, pg::PixelGrid, block)
     npg = PixelGrid(ul, lr, pg.origin, pg.transform)
     nodata = getnodatavalue(band)
     DataGrid{dtype}(offset, nodata, bs, Tuple(block), Tuple(block), npg)
-end
-
-
-"""
-Iterate over all blocks that cover a given pixel rectangle.
-"""
-function iter_block_indices(pg::PixelGrid, blocksize)
-    coarse_block_bounds = block_cover_ij_rect((pg.ul, pg.lr), blocksize)
-    CartesianIndices((coarse_block_bounds[1][1]:coarse_block_bounds[2][1],
-            coarse_block_bounds[1][2]:coarse_block_bounds[2][2]))
 end
 
 
@@ -286,7 +344,7 @@ function tune_band(fine_band, coarse_band, max_pixels)
             single_coarse_pg = onepixelgrid(coarse_crop_pg, pij)
             fine_cover_pg = crop_to(fine_pg, single_coarse_pg)
             if valid(fine_cover_pg)
-                fine_grid = load_pixel_grid(fine_band, fine_cover_pg)
+                fine_grid = load_data_grid(fine_band, fine_cover_pg)
                 pixel_cnt += 1
                 area = intersection_area(single_coarse_pg, fine_cover_pg)
                 @assert area >= 0
@@ -295,10 +353,11 @@ function tune_band(fine_band, coarse_band, max_pixels)
                     single_coarse_pg.ul[2]
                 ]
                 # This should be restricted to pixels under the coarse.
-                pixel_cnt = count_nonzero_pixels(fine_grid)
-                rate = max(landscan_value / pixel_cnt, 100)
+                pixel_cnt = count_nonzero_pixels(fine_grid, xy_bounds(single_coarse_pg))
+                @show pixel_cnt
+                rate = min(landscan_value / pixel_cnt, 100)
                 fill_nonzero_pixels!(fine_grid, rate)
-                write_datagrid(fine_grid, fine_band)
+                write_datagrid!(fine_grid, fine_band)
             else
                 outside_cnt += 1
             end
@@ -332,11 +391,11 @@ function assignweights(
         max_pixels = typemax(Int64)
         )
     # ds = dataset
-    fine_copy_ds = read(expanduser(points_path)) do fine_ds
+    fine_copy_ds = ArchGDAL.read(expanduser(points_path)) do fine_ds
         reweighted_path = expanduser(reweighted_path)
         ArchGDAL.copy(fine_ds, filename = reweighted_path)
     end
-    read(expanduser(weight_path)) do coarse_ds
+    ArchGDAL.read(expanduser(weight_path)) do coarse_ds
         fine_band = getband(fine_copy_ds, 1)
         coarse_band = getband(coarse_ds, 1)
         tune_band(fine_band, coarse_band, max_pixels)
--- test/test_assign_weights.jl
+++ test/test_assign_weights.jl
@@ -20,8 +20,8 @@ xy4 = geo_to_xy_center(origin, transform, 24, 36)
 @test xy4[1] ≈ xy3[1] + coarse_geo[2] / 2
 @test xy4[2] ≈ xy3[2] + coarse_geo[6] / 2 # Note that the offset is negative, -0.008333.
 
-
-xyb1 = xy_bounds(origin, transform, coarse_width_height)
+coarse_pg = PixelGrid((1, 1), coarse_width_height, geo_to_transform(coarse_geo)...)
+xyb1 = xy_bounds(coarse_pg)
 @test xyb1[1][1] ≈ -180
 @test xyb1[1][2] ≈ 90
 @test xyb1[2][1] ≈ 180
@@ -32,7 +32,7 @@ ij1 = pixel_containing(origin, transform, xy5)
 @test ij1[1] == 24
 @test ij1[2] == 36
 
-xyb2 = xy_bounds(origin, transform, coarse_width_height)
+xyb2 = xy_bounds(coarse_pg)
 # Nudge the corner just a hair so it isn't right on the boundary.
 xyb3 = ([xyb2[1][1], xyb2[1][2]], [xyb2[2][1] - 1e-5, xyb2[2][2] + 1e-5])
 ij_corners = ij_cover_rect(origin, transform, xyb3)
@@ -67,42 +67,71 @@ bl5 = block_cover_ij_rect(((204, 1), (250, 601)), (200, 300))
 @test bl5[2][1] == 2
 @test bl5[2][2] == 3
 
-pb1 = pixels_of_block(CartesianIndex(1, 1), (5, 3))
-@test length(pb1) == 15
-@test size(pb1) == (5, 3)
-@test pb1[1][1] == 1
-@test pb1[1][2] == 1
-@test pb1[15][1] == 5
-@test pb1[15][2] == 3
-cp1 = corner_pixel(CartesianIndex(1, 1), (5, 3))
-@test cp1 == pb1[1]
-pb2 = pixels_of_block(CartesianIndex(3, 2), (5, 3))
-@test length(pb2) == 15
-@test size(pb2) == (5, 3)
-@test pb2[1][1] == 11
-@test pb2[1][2] == 4
-@test pb2[15][1] == 15
-@test pb2[15][2] == 6
-cp2 = corner_pixel(CartesianIndex(3, 2), (5, 3))
-@test cp2 == pb2[1]
+rectish = [
+   ((1, 1), ((0, 0), (2, 2)), true),
+   ((3, 1), ((0, 0), (2, 4)), false),
+   ((1, 3), ((0, 0), (2, 4)), true),
+   ((-1, 1), ((0, 0), (2, 4)), false),
+   ((1, -1), ((0, 0), (2, 4)), false),
+   ((-1, -1), ((0, 0), (2, 4)), false),
+   ((3, 5), ((0, 0), (2, 4)), false),
+   ((1.5, 3.5), ((1, 3), (2, 4)), true),
+   ((.5, 3.5), ((1, 3), (2, 4)), false),
+   ((1.5, 2.5), ((1, 3), (2, 4)), false),
+]
 
+# Here's our stub setup.
+# Coarse grid from ul=(-10, 16) to lr=(10, 0) in xy
+# Give it w=20, h=16.
+# Fine grid from ul=(-5, 12) to lr=(5, 4) in xy
+# Give it w=10*5=50, h=8*5=40, so 5x the resolution of the coarse.
+coarse_band = StubBand{Int32}(
+   fill(400, 20, 16),
+   [6, 2],  # blocksize
+   Float64[-10, 16] , # origin
+   Float64[1 0.0; 0.0 -1],  # pixel size
+)
+fine_band = StubBand{Float64}(
+   fill(2.5, 50, 40),
+   [5, 3],  # blocksize
+   Float64[-5, 12] , # origin
+   Float64[.2 0.0; 0.0 -.2],  # pixel size
+)
+
+coarse_pg = pixelgrid(coarse_band)
+fine_pg = pixelgrid(fine_band)
+# This should be the center of the pixel.
+pc1 = pixel_containing(coarse_pg.origin, coarse_pg.transform, [-4.5, 11.5])
+@test pc1 == [6, 5]
+pc2 = pixel_containing(coarse_pg.origin, coarse_pg.transform, [-4.001, 11.001])
+@test pc2 == [6, 5]
+r1 = ij_cover_rect(coarse_pg.origin, coarse_pg.transform, ([-5, 12], [-4, 11]))
+@test r1[1] == [6, 5]
+@test r1[2][1] <= 7  # The cover rect can bring in a neighboring pixel.
+@test r1[2][2] <= 6
+r2 = ij_cover_rect(coarse_pg.origin, coarse_pg.transform, ([-4.999, 11.99], [4.999, 4.01]))
+@test r2 == ([6, 5], [15, 12])
+large_crop = crop_to(coarse_pg, fine_pg)
+
+sb1 = load_single_block(coarse_band, pg1, (1, 1))
+sb2 = load_single_block(coarse_band, pg1, (3, 3))
+@test sb2.A[13, 5] == 400
+# This is past the last block in x. There are 4 blocks.
+@test_throws AssertionError sb3 = load_single_block(coarse_band, pg1, (5, 3))
+
+# Load the whole thing.
+dg1 = load_data_grid(coarse_band, pg1)
+@test size(dg1.A) == (6 * 4, 16)  # Must be integral blocks to cover the domain.
+
+tune_band(fine_band, coarse_band, typemax(Int64))
+
+## A live data test.
 hrsl_path = "~/data/inputs/HRSL/uganda2018/hrsl_uga_pop.tif"
 landscan_path = "~/data/inputs/landscan/LandScan Global 2018/lspop2018/w001001.adf"
 hrsl_exists = isfile(expanduser(hrsl_path))
 landscan_exists = isfile(expanduser(landscan_path))
 
-if hrsl_exists && landscan_exists
-   ArchGDAL.read(expanduser(landscan_path)) do ls_ds
-      band = ArchGDAL.getband(ls_ds, 1)
-      pg1 = pixelgrid(band)
-
-      sb1 = load_single_block(band, pg1, (1, 1))
-      sb2 = load_single_block(band, pg1, (20, 20))
-
-      # Load the whole thing.
-      dg1 = load_pixel_grid(band, pg1)
-   end
-
+if hrsl_exists && landscan_exists && false
    out_path = "hrsl_ls_adjusted.tif"
    if ispath(out_path)
       rm(out_path)
